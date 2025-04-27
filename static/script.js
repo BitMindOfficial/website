67e33372721a07ff6a21a78fc1c5de500d380e99
MrLwitwma <147829440+MrLwitwma@users.noreply.github.com>
2025-04-27 22:55:11 +0530

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -1,3 +1,28 @@
 document.addEventListener('DOMContentLoaded', ()=>{
-    
+    const inputText = document.getElementById('word');
+    const sendBtn = document.getElementById('send');
+
+    sendBtn.addEventListener('click', ()=>{
+        sendWord()
+    })
+    inputText.addEventListener('keydown', (e)=>{
+        if (e.key === 'Enter'){
+            sendWord()
+        }
+    })
+
+    function sendWord(){
+        word = inputText.value
+        if (word.trim() != ''){
+            window.open(`https://dictionary.bitmindai.in?word=${word}`, "_blank");
+        } 
+    }
+
+
+    const video = document.getElementById('video');
+
+    video.addEventListener('ended', function() {
+        video.controls = true; // Show controls after video ends
+        video.muted = false;   // Optional: unmute after autoplay ends
+    });
 })
